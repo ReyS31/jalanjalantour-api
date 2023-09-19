ef2a14e29deff9827be1b504d0163b8295a3e6c8
Reyhan Pratama <rey.pratama123@gmail.com>
2023-09-19 16:24:34 +0800

    init

---
+++ config/plugins.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+    //
+    graphql: {
+      config: {
+        endpoint: '/graphql',
+        shadowCRUD: true,
+        playgroundAlways: false,
+        depthLimit: 12,
+        amountLimit: 100,
+        apolloServer: {
+          tracing: false,
+        },
+      },
+    },
+  };
---
+++ src/index.js
@@ -0,0 +1,71 @@
+"use strict";
+
+module.exports = {
+  /**
+   * An asynchronous register function that runs before
+   * your application is initialized.
+   *
+   * This gives you an opportunity to extend code.
+   */
+  register({ strapi }) {
+    const extensionService = strapi.service("plugin::graphql.extension");
+
+    extensionService.use(({ strapi }) => ({
+      typeDefs: `
+            type Query {
+              article(slug: String!): ArticleEntityResponse,
+              destination(slug: String!): DestinationEntityResponse
+            }
+          `,
+      resolvers: {
+        Query: {
+          article: {
+            resolve: async (parent, args, context) => {
+              const { toEntityResponse } = strapi.service(
+                "plugin::graphql.format"
+              ).returnTypes;
+
+              const data = await strapi.services["api::article.article"].find({
+                filters: { slug: args.slug },
+              });
+
+              const response = toEntityResponse(data.results[0]);
+
+              console.log("##################", response, "##################");
+
+              return response;
+            },
+          },
+          destination: {
+            resolve: async (parent, args, context) => {
+              const { toEntityResponse } = strapi.service(
+                "plugin::graphql.format"
+              ).returnTypes;
+
+              const data = await strapi.services[
+                "api::destination.destination"
+              ].find({
+                filters: { slug: args.slug },
+              });
+
+              const response = toEntityResponse(data.results[0]);
+
+              console.log("##################", response, "##################");
+
+              return response;
+            },
+          },
+        },
+      },
+    }));
+  },
+
+  /**
+   * An asynchronous bootstrap function that runs before
+   * your application gets started.
+   *
+   * This gives you an opportunity to set up your data model,
+   * run jobs, or perform some special logic.
+   */
+  bootstrap(/*{ strapi }*/) {},
+};
